e67510be863d90606dd838a81687b836e36569a0
Polina Grishkova <psgrishkova@ya.ru>
2021-07-05 22:48:48 +0400

    Initial commit

---
+++ src/main/java/com/example/Practice_GeneratingASchedule/PracticeGeneratingAScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Practice_GeneratingASchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGeneratingAScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGeneratingAScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Practice_GeneratingASchedule/PracticeGeneratingAScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Practice_GeneratingASchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGeneratingAScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
